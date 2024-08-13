3820627f05460fc7b6b455459e2556f0e22f1ed5
Ekaterina <ekaterinalife92@yandex.ru>
2024-08-13 19:02:22 +0300

    Init commit Сделаны главный экран в дневной и темной теме, экран с настройками (дневная и темная тема, кнопка перехода с главного экрана на экран с настройками

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        buttonSearch.setOnClickListener {
+
         }
+        val buttonMedia = findViewById<Button>(R.id.media)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+            }
+        }
+        buttonMedia.setOnClickListener(buttonClickListener)
+
+        val buttonSettings = findViewById<Button>(R.id.settings)
+        buttonSettings.setOnClickListener{
+            openSettings()
+        }
+    }
+    fun openSettings() {
+        val settingsIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(settingsIntent)
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_light)
+        findViewById<Button>(R.id.arrow).setOnClickListener {
+            finish()
+        }
+    }
+}
